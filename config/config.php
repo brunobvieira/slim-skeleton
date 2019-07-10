717fda5ffd6f2c9184c942d01b88685d4f877ac0
bruno.vieira <brunobeserravieira@gmail.com>
2019-07-10 11:47:00 -0300

    [NEW]  - Adding suport to .env file

--- config/config.php
+++ config/config.php
@@ -1,14 +1,16 @@
 <?php
 
+
 return [
     'settings' => [
         'displayErrorDetails' => true,
+        'secretKey' => getenv('SECRET_KEY') ?: 'secret_key',
         'db' => [
-            'driver' => 'mysql',
-            'host' => 'mysql-ck',
-            'database' => 'contact_keeper',
-            'username' => 'root',
-            'password' => '12345',
+            'driver' => getenv('DB_DRIVER') ?: 'mysql',
+            'host' => getenv('DB_HOST') ?: 'localhost',
+            'database' => getenv('DB_DATABASE') ?: 'default',
+            'username' => getenv('DB_USERNAME') ?: 'root',
+            'password' => getenv('DB_PASSWORD') ?: 'root',
         ]
     ]
 ];
