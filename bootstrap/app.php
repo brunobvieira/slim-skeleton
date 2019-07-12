c838b0da7dcedb1621459eb236132705d0653777
Bruno Beserra VIeira <brunobeserravieira@gmail.com>
2019-07-11 22:29:39 -0300

    - clear the code

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,9 +2,14 @@
 
 $container = $app->getContainer();
 
+
 // Instantiate the database
 require __DIR__ . '/db.php';
 
 
 // Declaring Routes
 require __DIR__ . '/../app/config/routes.php';
+
+
+// Declaring App Middlewares
+require __DIR__ . '/../app/config/middlewares.php';
